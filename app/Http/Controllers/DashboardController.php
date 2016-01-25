7ea64901cba492c7f1dc40d98a86f065e80118f8
Brian O'Sullivan <brian@tykus.ie>
2016-01-25 22:28:16 +0000

    Pass data into the dashboard view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,20 +9,21 @@ use App\Http\Controllers\Controller;
 use Auth;
 use App\Expectation;
 
-class DashboardController extends Controller 
+class DashboardController extends Controller
 {
 
-
   public function retrieve()
-    {
-    $expectations = Expectation::lists('name', 'id');
+  {
+    $expectations = Expectation::all();
+    $user = Auth::user();
 
-    	$user = Auth::user();
     if($user)
     {
-    return view('dashboard')->with(compact('user','expectations'));
-	} else{
-		   return redirect('auth/login');
+      return view('dashboard', compact('user','expectations'));
+    }
+    else
+    {
+      return redirect('auth/login');
+    }
+  }
 }
-  	}
- }
