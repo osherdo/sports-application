b9b069491dd84251d4bf8e55e5fb0509991a2870
Brian O'Sullivan <brian@tykus.ie>
2016-01-25 21:34:18 +0000

    Expectations model has no timestamps; define properties that can be mass-assigned

--- app/Expectation.php
+++ app/Expectation.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expectation extends Model
 {
-  
+
+  /**
+   * The expectations table does not contain 'created_at' or 'updated_at' fields
+   * @var boolean
+   */
+  public $timestamps = false;
+
+
+  /**
+   * Properties that cn be mass-assigned
+   * @var array
+   */
+  protected $fillable = ['name'];
+
 }
