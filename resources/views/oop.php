ddf2ed37a76f5e06547ca093467a4d5851360749
Osher <osherdo@gmail.com>
2016-01-30 22:50:38 +0200

    making progress with OOP

--- resources/views/oop.php
+++ resources/views/oop.php
@@ -216,4 +216,63 @@ unset($newobj); //generate messsage on browser.
 
 echo "<br><br>"."Sixth Drill"."<br><br>";
 echo "Preserving Original Method Functionality While Overwriting Methods:";
+//Using scope resolution operator (::):
+
+class MyClass
+{
+  public $prop1 = "I'm a class property!";
+
+  public function __construct()
+  {
+      echo 'The class "', __CLASS__, '" was initiated!<br />';
+  }
+
+  public function __destruct()
+  {
+      echo 'The class "', __CLASS__, '" was destroyed.<br />';
+  }
+
+  public function __toString()
+  {
+      echo "Using the toString method: ";
+      return $this->getProperty();
+  }
+
+  public function setProperty($newval)
+  {
+      $this->prop1 = $newval;
+  }
+
+  public function getProperty()
+  {
+      return $this->prop1 . "<br />";
+  }
+}
+
+class MyOtherClass extends MyClass
+{
+  public function __construct()
+  {
+      parent::__construct(); // Call the parent class's constructor
+      echo "A new constructor in " . __CLASS__ . ".<br />";
+  }
+
+  public function newMethod()
+  {
+      echo "From a new method in " . __CLASS__ . ".<br />";
+  }
+}
+
+// Create a new object
+$newobj = new MyOtherClass;
+
+// Output the object as a string
+echo $newobj->newMethod();
+
+// Use a method from the parent class
+echo $newobj->getProperty();
+
+unset($newobj); //generate messsage on browser.
+
+
   ?>
