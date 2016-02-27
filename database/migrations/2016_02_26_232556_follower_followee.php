825a0544e8aea7de94be190822786880c620f506
Osher <osherdo@gmail.com>
2016-02-27 21:30:18 +0200

    waiting for Johann's response

--- database/migrations/2016_02_26_232556_follower_followee.php
+++ database/migrations/2016_02_26_232556_follower_followee.php
@@ -28,9 +28,11 @@ class FollowerFollowee extends Migration
      *
      * @return void
      */
-    public function down()
-    
-{
-            //
-    }
+    public function down() 
+    { 
+        Schema::table('follower_followee', function (Blueprint $table)
+     { 
+        Schema::drop('follower_followee'); 
+  }); 
+}
 }
