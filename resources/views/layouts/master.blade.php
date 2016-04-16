250aa74a9184dd19470db9635e9a5e3730e84a6b
Osher <osherdo@gmail.com>
2016-04-16 18:16:50 +0300

    now inserting abdominals

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -65,7 +65,7 @@
         @yield('content')
 
 </div>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <script src="{{asset('js/bootstrap-multiselect.js')}}"></script>
 
     <!-- Script for the multi-select. -->
--- resources/views/routine.blade.php
+++ resources/views/routine.blade.php
@@ -31,7 +31,57 @@ Pick your routine type: {!! Form::select('type', ['Aerobic' => 'Aerobic', 'Anaer
 
 Give your routine a name: {!! Form::text('username'); !!}
 
+<header class="cd-header">
+	<h1>Now pick your routine exercises:</h1>
+</header>
+ 
+<main class="cd-main-content">
+	<div class="cd-tab-filter-wrapper">
+		<div class="cd-tab-filter">
+			<ul class="cd-filters">
+				<li class="placeholder"> 
+					<a data-type="all" href="#0">All</a> <!-- selected option on mobile -->
+				</li> 
+				<li class="filter"><a class="selected" href="#0" data-type="all">All</a></li>
+				<li class="filter" data-filter=".biceps"><a href="#0" data-type="color-1">Biceps</a></li>
+				<li class="filter" data-filter=".color-2"><a href="#0" data-type="color-2">Color 2</a></li>
+				<li class="filter" data-filter=".color-3"><a href="#0" data-type="color-3">Color 3</a></li>
+			</ul> <!-- cd-filters -->
+		</div> <!-- cd-tab-filter -->
+	</div> <!-- cd-tab-filter-wrapper -->
+ 
+	<section class="cd-gallery">
+		<ul>
+			<!--biceps exercises -->
+			<li class="mix biceps check1 radio1 option1"><img src="images/exercises/biceps/Alternate Hammer Curl.jpg" alt="Image 1"></li>
+			<li class="mix biceps check2 radio2 option2"><img src="images/exercises/biceps/Barbell Curl.jpg" alt="Image 2"></li>
+			<li class="mix biceps check3 radio3 option3"><img src="images/exercises/biceps/Barbell Curls Lying Against An Incline.jpg" alt="Image 3"></li>
+			<li class="mix biceps check4 radio4 option4"><img src="images/exercises/biceps/Cable Hammer Curls - Rope Attachment.jpg" alt="Image 4"></li>
+			<li class="mix biceps check5 radio5 option5"><img src="images/exercises/biceps/Close-Grip EZ-Bar Curl with Band.jpg" alt="Image 5"></li>
+			<li class="gap"></li>
+			<!--abdominals exercises -->
 
+
+		</ul>
+		<div class="cd-fail-message">No results found</div>
+	</section> <!-- cd-gallery -->
+ 
+	<div class="cd-filter">
+		<form>
+			<div class="cd-filter-block">
+				<h4>Block title</h4>
+				
+				<div class="cd-filter-content">
+					<!-- filter content -->
+				</div> <!-- cd-filter-content -->
+			</div> <!-- cd-filter-block -->
+		</form>
+ 
+		<a href="#0" class="cd-close">Close</a>
+	</div> <!-- cd-filter -->
+ 
+	<a href="#0" class="cd-filter-trigger">Filters</a>
+</main> <!-- cd-main-content -->
 
     
 {!! Form::close() !!}
