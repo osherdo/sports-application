87790f384aa9a95e04fb82b3594ea4fddfced8c7
Osher <osherdo@gmail.com>
2016-04-16 21:57:27 +0300

    done with exercises html code. Now need to create text box for each image

--- resources/views/routine.blade.php
+++ resources/views/routine.blade.php
@@ -43,9 +43,14 @@ Give your routine a name: {!! Form::text('username'); !!}
 					<a data-type="all" href="#0">All</a> <!-- selected option on mobile -->
 				</li> 
 				<li class="filter"><a class="selected" href="#0" data-type="all">All</a></li>
-				<li class="filter" data-filter=".biceps"><a href="#0" data-type="color-1">Biceps</a></li>
-				<li class="filter" data-filter=".color-2"><a href="#0" data-type="color-2">Color 2</a></li>
-				<li class="filter" data-filter=".color-3"><a href="#0" data-type="color-3">Color 3</a></li>
+				<li class="filter" data-filter=".biceps"><a href="#0" data-type="biceps">Biceps</a></li>
+				<li class="filter" data-filter=".abdominals"><a href="#0" data-type="abdominals">Abdominals</a></li>
+				<li class="filter" data-filter=".back"><a href="#0" data-type="back">Back</a></li>
+				<li class="filter" data-filter=".chest"><a href="#0" data-type="chest">Chest</a></li>
+				<li class="filter" data-filter=".legs"><a href="#0" data-type="legs">Legs</a></li>
+				<li class="filter" data-filter=".shoulders"><a href="#0" data-type="shoulders">Shoulders</a></li>
+				<li class="filter" data-filter=".triceps"><a href="#0" data-type="triceps">Triceps</a></li>
+				<li class="filter" data-filter=".test"><a href="#0" data-type="test">Test</a></li>
 			</ul> <!-- cd-filters -->
 		</div> <!-- cd-tab-filter -->
 	</div> <!-- cd-tab-filter-wrapper -->
@@ -53,13 +58,76 @@ Give your routine a name: {!! Form::text('username'); !!}
 	<section class="cd-gallery">
 		<ul>
 			<!--biceps exercises -->
+
 			<li class="mix biceps check1 radio1 option1"><img src="images/exercises/biceps/Alternate Hammer Curl.jpg" alt="Image 1"></li>
 			<li class="mix biceps check2 radio2 option2"><img src="images/exercises/biceps/Barbell Curl.jpg" alt="Image 2"></li>
 			<li class="mix biceps check3 radio3 option3"><img src="images/exercises/biceps/Barbell Curls Lying Against An Incline.jpg" alt="Image 3"></li>
 			<li class="mix biceps check4 radio4 option4"><img src="images/exercises/biceps/Cable Hammer Curls - Rope Attachment.jpg" alt="Image 4"></li>
 			<li class="mix biceps check5 radio5 option5"><img src="images/exercises/biceps/Close-Grip EZ-Bar Curl with Band.jpg" alt="Image 5"></li>
-			<li class="gap"></li>
+			<!-- <li class="gap"></li> --> <!-- Creating a gap on abdominals section -->
+
 			<!--abdominals exercises -->
+
+			<li class="mix abdominals check1 radio1 option1"><img src="images/exercises/abdominals/bottoms up.jpg" alt="Image 1"></li>
+			<li class="mix abdominals check1 radio1 option1"><img src="images/exercises/abdominals/Frog Sit-Ups.jpg" alt="Image 1"></li>
+			<li class="mix abdominals check1 radio1 option1"><img src="images/exercises/abdominals/Hanging Oblique Knee Raise.jpg" alt="Image 1"></li>
+			<li class="mix abdominals check1 radio1 option1"><img src="images/exercises/abdominals/plank.jpg" alt="Image 1"></li>
+			<li class="mix abdominals check1 radio1 option1"><img src="images/exercises/abdominals/Wind Sprints.jpg	" alt="Image 1"></li>
+
+			<!--back exercises -->
+
+			<li class="mix back check1 radio1 option1"><img src="images/exercises/back/alternating renegade row.jpg" alt="Image 1"></li>
+			<li class="mix back check1 radio1 option1"><img src="images/exercises/back/Bent Over One-Arm Long Bar Row.jpg" alt="Image 1"></li>
+			<li class="mix back check1 radio1 option1"><img src="images/exercises/back/Bent Over Two-Dumbbell Row With Palms In.jpg" alt="Image 1"></li>
+			<li class="mix back check1 radio1 option1"><img src="images/exercises/back/Incline Bench Pull.jpg" alt="Image 1"></li>
+			<li class="mix back check1 radio1 option1"><img src="images/exercises/back/Mixed Grip Chin.jpg" alt="Image 1"></li>
+
+			<!-- chest exercises -->
+
+			<li class="mix chest check1 radio1 option1"><img src="images/exercises/chest/Barbell Bench Press - Medium Grip.jpg" alt="Image 1"></li>
+			<li class="mix chest check1 radio1 option1"><img src="images/exercises/chest/Cable Crossover.jpg" alt="Image 1"></li>
+
+			<li class="mix chest check1 radio1 option1"><img src="images/exercises/chest/Dips.jpg" alt="Image 1"></li>
+
+			<li class="mix chest check1 radio1 option1"><img src="images/exercises/chest/Dumbbell Bench Press.jpg" alt="Image 1"></li>
+
+			<li class="mix chest check1 radio1 option1"><img src="images/exercises/chest/Pushups.jpg" alt="Image 1"></li>
+
+			<!--legs exercises -->
+
+			<li class="mix legs check1 radio1 option1"><img src="images/exercises/legs/Barbell Full Squat.jpg" alt="Image 1"></li>
+			<li class="mix legs check1 radio1 option1"><img src="images/exercises/legs/Barbell Lunge.jpg" alt="Image 1"></li>
+
+			<li class="mix legs check1 radio1 option1"><img src="images/exercises/legs/Barbell Walking Lung.jpg" alt="Image 1"></li>
+
+			<li class="mix legs check1 radio1 option1"><img src="images/exercises/legs/Front Squats With Two Kettlebells.jpg" alt="Image 1"></li>
+
+			<li class="mix legs check1 radio1 option1"><img src="images/exercises/legs/Narrow Stance Leg Press.jpg" alt="Image 1"></li>
+
+			<!--shoulders exercises -->
+
+			<li class="mix shoulders check1 radio1 option1"><img src="images/exercises/shoulders/Dumbbell Lying One-Arm Rear Lateral Raise.jpg" alt="Image 1"></li>
+			<li class="mix shoulders check1 radio1 option1"><img src="images/exercises/shoulders/Reverse Flyes.jpg" alt="Image 1"></li>
+
+			<li class="mix shoulders check1 radio1 option1"><img src="images/exercises/shoulders/Side Laterals to Front Raise.jpg" alt="Image 1"></li>
+
+			<li class="mix shoulders check1 radio1 option1"><img src="images/exercises/shoulders/Single-Arm Linear Jammer.jpg" alt="Image 1"></li>
+
+			<li class="mix shoulders check1 radio1 option1"><img src="images/exercises/shoulders/Standing Palm-In One-Arm Dumbbell Press.jpg" alt="Image 1"></li>
+
+			<!-- triceps exercises -->
+
+			<li class="mix triceps check1 radio1 option1"><img src="images/exercises/triceps/Decline EZ Bar Triceps Extension.jpg" alt="Image 1"></li>
+			<li class="mix triceps check1 radio1 option1"><img src="images/exercises/triceps/Parallel Bar Dip.jpg" alt="Image 1"></li>
+
+			<li class="mix triceps check1 radio1 option1"><img src="images/exercises/triceps/Push-Ups - Close Triceps Position.jpg" alt="Image 1"></li>
+
+			<li class="mix triceps check1 radio1 option1"><img src="images/exercises/triceps/Seated Triceps Press.jpg" alt="Image 1"></li>
+
+			<li class="mix triceps check1 radio1 option1"><img src="images/exercises/triceps/Weighted Bench Dip.jpg" alt="Image 1"></li>
+
+
+
 
 
 		</ul>
